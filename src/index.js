ebfab4f794482c10e21ed45ae7130348a69cfb7a
Kateryna Artemieva <moruga.katya@gmail.com>
2023-05-29 12:42:13 +0100

    end

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-
+import throttle from 'lodash.throttle';
+// Accordion
 new Accordion('.accordion-container');
+// Swiper
 var swiper = new Swiper('.mySwiper', {
   cssMode: true,
   navigation: {
@@ -14,12 +16,9 @@ var swiper = new Swiper('.mySwiper', {
   mousewheel: true,
   keyboard: true,
 });
-
-// відкривання і закривання мобільної менюшки
-// (() => {
+// mobileMenu
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
-console.log(openMenuBtn);
 const closeMenuBtn = document.querySelector('.js-close-menu');
 const bodyScroll = document.querySelector('body');
 
@@ -31,26 +30,35 @@ function onOpenClick() {
   bodyScroll.classList.add('no-scroll');
   closeMenuBtn.addEventListener('click', onCloseClick);
 }
-function onCloseClick (){
-    mobileMenu.classList.remove('is-open');
+function onCloseClick() {
+  mobileMenu.classList.remove('is-open');
   openMenuBtn.classList.remove('is-open');
   closeMenuBtn.classList.remove('is-open');
   bodyScroll.classList.remove('no-scroll');
   openMenuBtn.addEventListener('click', onOpenClick);
-
 }
-// const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+// Hero
+const hero = document.querySelector('.hero');
+const men = document.querySelector('.js-men');
+const imgBaner1 = document.querySelector('.js-img-1');
+const imgBaner2 = document.querySelector('.js-img-2');
 
-//     mobileMenu.classList.toggle('is-open');
-//     openMenuBtn.classList.toggle('is-open');
-//     closeMenuBtn.classList.toggle('is-open');
-//     // navigation.classList.toggle('mobile');
-//     // authPerson.classList.toggle('mobile');
-//     // signUpUser.classList.toggle('mobile');
+const imgBaner3 = document.querySelector('.js-img-3');
+const imgBaner4 = document.querySelector('.js-img-4');
 
-//     bodyScroll.classList.toggle('no-scroll');
-// }
-// })
+hero.addEventListener('mousemove', throttle(onHeroMove, 60));
+function onHeroMove(evt) {
+  console.dir(evt);
+  let xHero = evt.clientX;
+  let yHero = evt.clientY;
+  men.style.transform =
+    'translate(' + xHero * 0.1 * -1 + 'px, ' + yHero * 0.1 * -1 + 'px)';
+  imgBaner1.style.transform =
+    'translate(' + xHero * 0.05 * -1 + 'px, ' + yHero * 0.05 * -1 + 'px)';
+  imgBaner2.style.transform =
+    'translate(' + xHero * 0.05 * -1 + 'px, ' + yHero * 0.05 * -1 + 'px)';
+  imgBaner3.style.transform =
+    'translate(' + xHero * 0.05 * -1 + 'px, ' + yHero * 0.05 * -1 + 'px)';
+  imgBaner4.style.transform =
+    'translate(' + xHero * 0.05 * -1 + 'px, ' + yHero * 0.05 * -1 + 'px)';
+}
