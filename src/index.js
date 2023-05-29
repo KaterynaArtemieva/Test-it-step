d9877bace852d2517cb600fd3c27debdda73bf5d
Kateryna Artemieva <moruga.katya@gmail.com>
2023-05-29 05:47:13 +0100

    +swiper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
- new Accordion('.accordion-container');
+new Accordion('.accordion-container');
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
